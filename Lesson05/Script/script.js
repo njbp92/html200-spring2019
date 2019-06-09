866aec9ea06139afaef464c6325c79531a6f43ba
Nataly <njbp92@gmail.com>
2019-06-09 13:38:55 -0700

    Add files via upload

--- Lesson05/Script/script.js
+++ Lesson05/Script/script.js
@@ -1,29 +1,58 @@
 let input = prompt ("Bank Options: Enter the letter for the selected command: Q to Quit, W to Withdraw, D to Deposit, B to check balance. ");
-
+let balance = 0.0;
 
 const bankTasks = '';
 
 function ATM(options) {
   switch (options) {
     case 'Q': {
-      return 'Are you sure you want to quit?';
+      return Quit ();
       break;
-      }
+    }
+      case 'q': {
+        return Quit ();
+        break;
+        }
     case 'W': {
-      return 'How much would you like to take out?';
+      return Withdraw ();
       break;
-      }
+    }
+      case 'w': {
+        return Withdraw ();
+
+        break;
+        }
     case 'D': {
-      return 'Enter the amount of the deposit';
+      return Deposit ();
       break;
       }
+      case 'd': {
+        return Deposit ();
+        break;
+        }
     case 'B': {
-      return 'Balance is...';
+      return Balance ();
       break;
+      }
+      defult: {
+        return Quit ();
       }
     }
   }
+  function Withdraw () {
+   prompt ('How much would you like to take out?');
+   return 'balance - Withdraw ('');
+  }
 
+  function Deposit () {
+   prompt ('How much would you like to deposit?');
+   return 'balance - Deposit ('')';
+  }
+
+  function Deposit () {
+  prompt ('Here is your balance');
+   return 'balance';
+  }
 console.log(
   ATM(bankTasks)
-);
+)
