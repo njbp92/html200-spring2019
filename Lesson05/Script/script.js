b6c11d0fc7ef0e2fd68342a7ac62b3b9388525f7
Nataly <njbp92@gmail.com>
2019-06-10 20:09:32 -0700

    Add files via upload

--- Lesson05/Script/script.js
+++ Lesson05/Script/script.js
@@ -1,57 +1,74 @@
-let input = prompt ("Bank Options: Enter the letter for the selected command: Q to Quit, W to Withdraw, D to Deposit, B to check balance. ");
+let input = prompt("Bank Options: Enter the letter for the selected command: Q to Quit, W to Withdraw, D to Deposit, B to check balance.");
 let balance = 0.0;
 
-const bankTasks = '';
+function askUser() {
+	input = prompt("Bank Options: Enter the letter for the selected command: Q to Quit, W to Withdraw, D to Deposit, B to check balance.");
+}
 
-function ATM(options) {
-  switch (options) {
-    case 'Q': {
-      return Quit ();
-      break;
-    }
-      case 'q': {
-        return Quit ();
-        break;
-        }
-    case 'W': {
-      return Withdraw ();
-      break;
-    }
-      case 'w': {
-        return Withdraw ();
-        break;
-        }
-    case 'D': {
-      return Deposit ();
-      break;
-      }
-      case 'd': {
-        return Deposit ();
-        break;
-        }
-    case 'B': {
-      return Balance ();
-      break;
-      }
-      defult: {
-        return Quit ();
-      }
-    }
+do {
+	switch (input) {
+		case "Q":
+			{
+				Quit();
+				break;
+			}
+		case "q":
+			{
+				Quit();
+				break;
+			}
+		case "W":
+			{
+				Withdraw();
+				break;
+			}
+		case "w":
+			{
+				Withdraw();
+				break;
+			}
+		case "D":
+			{
+				Deposit();
+				break;
+			}
+		case "d":
+			{
+				Deposit();
+				break;
+			}
+		case "B":
+			{
+				Balance();
+				break;
+			}
+		default:
+			{
+			alert('Please enter a valid option')
+      input;
+			}
   }
-  function Withdraw() {
-   prompt ('How much would you like to take out?');
-   return 'balance - Withdraw ('')';
-  }
+}
 
-  function Deposit() {
-   prompt ('How much would you like to deposit?');
-   return 'balance - Deposit ('')';
-  }
+while (answer != 'Q');
 
-  function Balance() {
-  prompt ('Here is your balance');
-   return 'balance';
-  }
-console.log(
-  ATM(bankTasks)
-)
+function Balance() {
+	alert(balance);
+}
+
+function Withdraw() {
+	num = prompt('How much would you like to take out?');
+	alert(balance - num);
+}
+
+function Deposit() {
+	num = prompt('How much would you like to deposit?');
+	alert(balance + num);
+}
+
+function Quit() {
+	prompt('Are you sure you want to quit?');
+}
+
+
+console.log(ATM(bankTasks))
